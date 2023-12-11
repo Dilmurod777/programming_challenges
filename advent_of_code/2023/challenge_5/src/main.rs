84135516fb988e74721833d956b2553e89df4954
Dilmurod Valiev <valiev.dilmurod1998@gmail.com>
2023-12-11 17:41:38 +0900

    complete challenge part 1

--- advent_of_code/2023/challenge_5/src/main.rs
+++ advent_of_code/2023/challenge_5/src/main.rs
@@ -15,7 +15,62 @@ fn main() {
         .map(|x| x.trim())
         .collect();
 
+    let mut values: Vec<i64> = Vec::new();
+    let mut ranges: Vec<Vec<i64>> = Vec::new();
+
     for i in 0..lines.len() {
         let line = lines[i];
+
+        if line.is_empty() {
+            continue;
+        }
+
+        if line.starts_with("seeds") {
+            let parts: Vec<&str> = line.split(":").collect();
+            values = parts[1].trim().split(" ").map(|x| x.parse::<i64>().unwrap()).collect();
+            continue;
+        }
+
+        if !line.contains("map:") {
+            let parts: Vec<i64> = line.split(" ").map(|x| x.parse::<i64>().unwrap()).collect();
+            ranges.push(parts);
+        } else {
+            for j in 0..values.len() {
+                for range in &ranges {
+                    let source = range[1];
+                    let destination = range[0];
+                    let count = range[2];
+
+                    if values[j] >= source && values[j] < source + count {
+                        values[j] = destination + values[j] - source;
+                        break;
+                    }
+                }
+            }
+
+            ranges.clear();
+        }
+    }
+
+    for j in 0..values.len() {
+        for range in &ranges {
+            let source = range[1];
+            let destination = range[0];
+            let count = range[2];
+
+            if values[j] >= source && values[j] < source + count {
+                values[j] = destination + values[j] - source;
+                break;
+            }
+        }
+    }
+
+    ranges.clear();
+
+    match values.iter().min() {
+        None => {}
+        Some(x) => {
+            println!("min: {}", x);
+        }
     }
 }
