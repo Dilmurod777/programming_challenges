d96a925b0761e371b096d545895462824db42fa5
Dilmurod Valiev <valiev.dilmurod1998@gmail.com>
2023-12-14 13:52:33 +0900

    complete challenge

--- advent_of_code/2023/challenge_8/src/main.rs
+++ advent_of_code/2023/challenge_8/src/main.rs
@@ -1,8 +1,32 @@
 use std::collections::HashMap;
 use std::fs;
 
+fn lcm(first: usize, second: usize) -> usize {
+    first * second / gcd(first, second)
+}
+
+fn gcd(first: usize, second: usize) -> usize {
+    let mut max = first;
+    let mut min = second;
+    if min > max {
+        let val = max;
+        max = min;
+        min = val;
+    }
+
+    loop {
+        let res = max % min;
+        if res == 0 {
+            return min;
+        }
+
+        max = min;
+        min = res;
+    }
+}
+
 fn main() {
-    let input_filename = "input3.txt";
+    let input_filename = "input4.txt";
     let output_filename = "output.txt";
 
     let _ = fs::remove_file(output_filename);
@@ -24,6 +48,8 @@ fn main() {
         };
     }).collect();
 
+    let mut current_locations: Vec<&str> = Vec::new();
+
     for i in 2..lines.len() {
         let line = lines[i];
         let parts: Vec<&str> = line.split("=").collect();
@@ -37,20 +63,33 @@ fn main() {
             .collect();
 
         routes.entry(location.to_string()).or_insert(next_locations);
+
+        if location.ends_with("A") {
+            current_locations.push(location);
+        }
     }
 
-    let mut current_location = "AAA";
-    let final_location = "ZZZ";
-    let mut steps = 0;
-    let mut current_direction_index = 0;
+    let mut steps = vec![0; current_locations.len()];
+    let mut current_direction_index: usize;
 
-    while current_location != final_location {
-        let current_direction = directions[current_direction_index % directions.len()] as usize;
-        let next_locations = routes.get(current_location).unwrap();
-        current_location = next_locations[current_direction].as_str();
-        steps += 1;
-        current_direction_index += 1;
+    for i in 0..current_locations.len() {
+        current_direction_index = 0;
+        while !current_locations[i].ends_with("Z") {
+            let current_direction = directions[current_direction_index % directions.len()] as usize;
+
+            let next_locations = routes.get(current_locations[i]).unwrap();
+            current_locations[i] = next_locations[current_direction].as_str();
+            steps[i] += 1;
+            current_direction_index += 1;
+        }
     }
 
-    println!("{:?}", steps);
+    while steps.len() != 1 {
+        let a = steps.pop().unwrap();
+        let b = steps.pop().unwrap();
+
+        steps.push(lcm(a, b));
+    }
+
+    println!("{:?}", steps[0]);
 }
